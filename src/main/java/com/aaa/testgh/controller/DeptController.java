2c160cf78e6e9b59f39e1588e200b48ba26904f6
wyl331274463 <w331274463@qq.com>
2018-11-29 09:32:51 +0800

    Initial commit

---
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,14 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:wuyanle
+ * createTime:2018-11-29 09:07
+ */
+public class DeptController {
+    public static void main(String[] args) {
+        System.out.println("测试github提交111");
+    }
+}
+
